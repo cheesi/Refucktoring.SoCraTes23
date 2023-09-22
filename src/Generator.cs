01ad0ffe5b5b4f6552557afc657e0f54286470c5
Christian Schabetsberger <christian.schabetsberger@finxit.at>
2023-09-22 19:06:13 +0200

    magic strings are eval. use consts!

--- src/Generator.cs
+++ src/Generator.cs
@@ -2,19 +2,23 @@
 
 public static class Generator
 {
+    private const string fizz = "Buzz";
+    private static string bUzZ = "Fizz";
+    private static string buzzFIZZ = bUzZ + fizz;
+
     public static string Generate(int number)
     {
         if (number % 3 == 0 && number % 5 == 0)
         {
-            return "FizzBuzz";
+            return buzzFIZZ;
         }
         else if (number % 3 == 0)
         {
-            return "Fizz";
+            return bUzZ;
         }
         else if (number % 5 == 0)
         {
-            return "Buzz";
+            return fizz;
         }
         else
         {
